2b829e917d1ed0c47c2173ef9286c16d9bba8968
nthanniru <62169751+nthanniru@users.noreply.github.com>
2022-06-24 23:46:59 +0530

    Update variables.tf

--- EC2withJenkins/variables.tf
+++ EC2withJenkins/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  type    = "string"
+  type    = string
   default = "us-east-1"
 }
 variable "ami_id" {
@@ -9,10 +9,10 @@ variable "ami_id" {
   }
 }
 variable "instance_type" {
-  type    = "string"
+  type    = string
   default = "t2.micro"
 }
 variable "key_name" {
-  type    = "string"
+  type    = string
   default = "ec2-demo"
 }
